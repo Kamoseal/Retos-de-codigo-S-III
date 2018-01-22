ea29137daafad4666a4fc86a0cf728036def5153
kamoseal <serna.karla@gmail.com>
2018-01-22 20:35:36 +0000

    readme

---
+++ README.md
@@ -0,0 +1,18 @@
+L Y F T
+Desarrolla una web-app que replique el sitio de Lyft, en este reto deberás cumplir los pasos necesarios para que tu usuario pueda registrarse.
+
+FLUJO DE LA APLICACIÓN.
+Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista de inicio. La vista de inicio cuenta con dos botones, en esta ocasión seguiremos el flujo de SING UP.
+
+En la siguiente vista tenemos un formulario donde nuestro usuario puede escoger el país y debe ingresar su número de teléfono. El botón de NEXT debe estar deshabilitado hasta que se ingrese un número de 10 dígitos.
+
+Una vez ingresado el número de teléfono se habilita el botón y al dar click debe enviar una alerta con un código generado aleatoriamente (LAB-000) y redireccionar a la siguiente vista.
+
+En esta vista se debe ingresar el código dado anteriormente y una vez hecho esto se habilita el botón que redirecciona a nuestro usuario a la vista donde ingresa sus datos. (Puede tener la opción de enviar otro código.)
+
+Para ingresar sus datos necesitamos un formulario que le pida su nombre, apellido y correo electrónico. Deberá también tener un checkbox para que se acepten los términos y condiciones del servicio.
+
+Ya que se ha realizado lo anterior, sólo se deberá mostrar una vista al usuario que le indique que ha concluido con el registro exitosamente.
+
+En el siguiente link puedes visualizar el diagrama esperado
+https://lms.laboratoria.la/cohorts/cdmx-2017-10-bc-core-pm/courses/social-network/02-jquery/08-code-challenges
--- js/app.js
+++ js/app.js
@@ -7,22 +7,68 @@ $(document).ready(function(){
   //Ocultar y mostrar sing
   $("#sing").click(function() {
     $("#welcome").slideUp(1000);
-  })
+  });
   //Mostrat pagina anterior
-  $("#btnLeft").click(function(){
+  $(".btnLeft").click(function(){
     $("#welcome").slideDown(1000);
-  })
+  });
 //Insertando lada a input
-  $("#option").click(function() {
-    if ($(this)===($(".mexico"))){
-      $("#contryPhone").html("<b>52</b>");
+  $("#optionS").change(function() {
+
+    if ($(this).val()===("1")){
+      $("#contryPhone").val("52");
     }
-    else if ($(this)===($(".usa"))){
-      $("#contryPhone").html("01");
+    else if ($(this).val()===("2")){
+      $("#contryPhone").val("01");
     }
-    else if ($(this)===($(".peru"))){
-      $("#contryPhone").html("51");
+    else if ($(this).val()===("3")){
+      $("#contryPhone").val("51");
+    }
+  });
+
+  $("#getCode, #resendCode").click(function (event){
+    event.preventDefault();
+    function generateCode() {
+      var code = (Math.floor((Math.random() * 1000)));
+      return code;
+    }
+    var generatedCode = parseInt(generateCode());
+    alert("Tu código: LAB-" + generatedCode);
+    $("input[name=verify]").attr({
+      "data-code": generatedCode,
+    });
+    $("#getPhone").slideUp(1000);
+  });
+
+  $("#contryPhone").keyUp(function() {
+    if ($(this).val().length!==12){
+        $("#getCode").attr({
+          disabled
+          });
+    }else{
+
     }
   })
+
+})
+  $("#btnLeft").click(function(){
+    $("#getPhone").slideDown(1000);
+
+  $("input[name=verify]").click(function(){
+  var data= $("input[data-code]").attr();
+  console.log(data);
+});
+  // $("verifyNums").keypress(function(event){
+  //   event.preventDefault();
+  //   if ($(this).attr("data"))===($(this).val()){
+  //     return true;
+  //   }
+  //   else{
+  //     alert ("¡Intentalo de nuevo!")
+  //   }
+  // })
+  // $("#getPhone").slideUp(1000);
+
+
 
   });
