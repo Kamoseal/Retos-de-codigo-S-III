536821c15ec28a18f614e0f7aa141221d9da2d22
kamoseal <serna.karla@gmail.com>
2018-01-22 03:59:35 -0600

    pidiendo telefono

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
-var principal = $("main");
-var welcome = $("#welcome");
+
+
+$(document).ready(function(){
+  //ocultar main
+  $("main").slideUp(4000);
+
+  //Ocultar y mostrar sing
+  $("#sing").click(function() {
+    $("#welcome").slideUp(1000);
+  })
+  //Mostrat pagina anterior
+  $("#btnLeft").click(function(){
+    $("#welcome").slideDown(1000);
+  })
+//Insertando lada a input
+  $("#option").click(function() {
+    if ($(this)===($(".mexico"))){
+      $("#contryPhone").html("<b>52</b>");
+    }
+    else if ($(this)===($(".usa"))){
+      $("#contryPhone").html("01");
+    }
+    else if ($(this)===($(".peru"))){
+      $("#contryPhone").html("51");
+    }
+  })
+
+  });
